09ea37787079aafb0309b3455790fdad27e355a8
coffeeholic01 <won.jung001@kaist.ac.kr>
2022-10-05 19:08:52 +0900

    Update news_trends

--- news_contents_crawler.py
+++ news_contents_crawler.py
@@ -1,97 +1,94 @@
 import pdb
-import boto3
 import time
-import datetime as dt 
 import json
+import boto3
 import requests
+import datetime as dt
+import warnings
+import re
 
 from bs4 import BeautifulSoup
-import re
 from config import *
 
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 def fetch_news_contents(msg):
-    #print(msg)
-    #print(msg.message_id)
-    #print(msg.body)
+    # print(msg)
+    # print(msg.message_id)
+    # print(msg.body)
+
     item = json.loads(msg.body)
-    #print(item)
+    # print(item)
 
     headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
 
     resp = requests.get(item['url'], headers=headers)
 
-    assert resp.status_code == 200
+    # assert resp.status_code == 200
+    if resp.status_code != 200:
+        return None
 
     soup = BeautifulSoup(resp.text, 'html.parser')
 
-    node_ = soup.find('meta', {'property':'og:article:author:url'})
-    if node_ != None:
-        if node_['content'] == "https://sports.news.naver.com":
-             return 
-    
-    node = soup.find("meta", {"property":"og:article:author"})
-    
+    node = soup.find("meta", {"property": "og:article:author"})
     if node:
-        #print('content:' + node['content'])
-        tokens = node['content'].split('|')
-        #print(tokens[0], tokens[1])
+        content = node['content']
 
-        if '네이버 스포츠' in tokens[0]:
+        if '네이버 스포츠' in content:
+            return None
+        if 'TV연예' in content:
             return None
 
-        elif 'TV연예' in tokens[1]:
-            return None
-
+        tokens = content.split('|')
         publisher = tokens[0].strip()
 
     else:
-        print('1111')
         pdb.set_trace()
+
+    # print(publisher)
 
     datestr_list, source_url = parse_media_info(soup)
 
-    #print(datestr_list)
-    #print(source_url)
-    
+    # print(datestr_list)
+    # print(source_url)
+
     if len(datestr_list) == 1:
         created_at = parse_datestr(datestr_list[0])
         updated_at = created_at
-    
+
     elif len(datestr_list) == 2:
         created_at = parse_datestr(datestr_list[0])
         updated_at = parse_datestr(datestr_list[1])
 
     else:
         print(item)
-        print(2222)
         pdb.set_trace()
 
+    # print(created_at)
+    # print(updated_at)
 
-    body = soup.find("div", {"id":"newsct_article"})
+    body = soup.find("div", {"id": "newsct_article"})
 
     assert body is not None
 
     body_text = body.text.strip()
-
-    #print(body_text)
+    # print(body_text)
 
     images = body.find_all("img")
-    image_urls = []
-    for x in images:
-        if x.has_attr('src'):
-            image_urls.append(x['src'])
-        else:
-            image_urls.append(x['data-src'])
-    #image_urls = [x['data-src'] for x in images]
-    image_urls = [re.sub(r'\?[\w=]+', '', x) for x in image_urls]
-    image_urls = list(set(image_urls))
+    try:
+        image_urls = [x['data-src'] for x in images]
+        image_urls = [re.sub(r'\?[\w=]+', '', x) for x in image_urls]   # x 안에서 정규식모양의 것을 ''로 바꿔라
+        image_urls = list(set(image_urls))  # 중복 제거
+    except:
+        image_urls = []
 
-    #print(image_urls)
+    # print(image_urls)
 
-    byline = soup.find("span", {"class":"byline_s"})
+    byline = soup.find("span", {"class": "byline_s"})
     reporter_name, reporter_email = extract_reporter(byline)
 
+    # print(reporter_name)
+    # print(reporter_email)
 
     entry = {
         'id': item['msg_id'],
@@ -104,31 +101,26 @@ def fetch_news_contents(msg):
         'created_at': created_at.isoformat(),
         'updated_at': updated_at.isoformat(),
         'reporter_name': reporter_name,
-        'reporter_email':reporter_email,
+        'reporter_email': reporter_email,
         'body': body_text,
     }
-    
+
+    # print(entry)
+
     return entry
 
-    #pdb.set_trace()
-
 def parse_media_info(soup):
-    media_info = soup.find("div", {"class":"media_end_head_info_datestamp"})
+    media_info = soup.find("div", {"class": "media_end_head_info_datestamp"})
 
     if media_info:
-        datestr_list = media_info.find_all("span", {"class":"media_end_head_info_datestamp_time"})
+        datestr_list = media_info.find_all("span", {"class": "media_end_head_info_datestamp_time"})
 
-        link = media_info.find("a",{"class":"media_end_head_origin_link"})
+        link = media_info.find("a", {"class": "media_end_head_origin_link"})
         source_url = link['href'] if link else ''
 
         return datestr_list, source_url
-
-    else:
-        pass 
-        print('sports_news')
-        print()
-    #pdb.set_trace()
-
+    pass
+    
 def parse_datestr(span):
     if span.has_attr('data-date-time'):
         datestr = span['data-date-time']
@@ -141,28 +133,23 @@ def parse_datestr(span):
 
     return date
 
-    pdb.set_trace()
-
-
 def extract_reporter(byline):
     if byline is None:
         return '', ''
 
-    byline = byline.text 
-    #print(byline)
+    byline = byline.text
 
     m = re.match(r'([\wㄱ-ㅎ가-힣]+)\s*(기자)?\s*\(?([\w\.]+@[\w\.]+)\)?', byline)
     if m:
-        #print (m[1], m[3])
         return m[1], m[3]
 
     m = re.match(r'([\wㄱ-ㅎ가-힣]+)\s*(기자)?', byline)
     if m:
-       # print (m[1], '')
         return m[1], ''
 
-    else:
-        pass 
+    print(byline)
+
+    return '', ''
 
 def upload_to_elastic_search(buffer):
     if len(buffer) == 0:
@@ -176,13 +163,12 @@ def upload_to_elastic_search(buffer):
                 '_id': x['id']
             }
         }
-    
+
         data += json.dumps(index) + '\n'
         data += json.dumps(x) + '\n'
 
-        
     headers = {
-        'Content-Type' : 'application/json'
+        'Content-Type': 'application/json'
     }
 
     resp = requests.post(
@@ -192,9 +178,9 @@ def upload_to_elastic_search(buffer):
         auth=ELASTICSEARCH_AUTH
     )
 
-    #print(resp.status_code)
+    # print(resp.status_code)
+
     assert resp.status_code == 200
-    #pdb.set_trace()
 
 
 if __name__ == '__main__':
@@ -210,13 +196,15 @@ if __name__ == '__main__':
             WaitTimeSeconds=1,
         )
 
+        # print(messages)
+
         if len(messages) == 0:
-            print(' - Queue is empty. Wait for a while.')
+            print('- Queue is empty. Wait for a while.')
             time.sleep(60)
             continue
 
-        #for msg in messages:
-        #    msg.delete()
+        for msg in messages:
+            msg.delete()
 
         buffer = []
 
@@ -224,12 +212,13 @@ if __name__ == '__main__':
             print('.', end='', flush=True)
 
             entry = fetch_news_contents(msg)
-            
+
             if entry:
                 buffer.append(entry)
-        
+
         upload_to_elastic_search(buffer)
 
         print('!!')
 
-        time.sleep(3)
+        # time.sleep(2)
+
